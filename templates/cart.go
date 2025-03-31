536603ec344ac162c1040031d65ab1dff76d7047
Densest Void <me@densestvoid.dev>
2025-03-30 22:02:20 -0400

    Remove Kroger primary branding given indirect monetization (#25)

--- templates/cart.go
+++ templates/cart.go
@@ -34,9 +34,7 @@ func Cart() gomponents.Node {
 				html.Button(
 					html.Type("button"),
 					html.Class("btn btn-primary"),
-					gomponents.Text("Send to "),
-					html.Img(html.Src("https://developer.kroger.com/assets/logos/kroger.svg")),
-					gomponents.Text(" cart"),
+					gomponents.Text("Send to Kroger cart"),
 					htmx.Post("/cart/checkout"),
 					htmx.Swap("none"),
 					htmx.Trigger("click"),
